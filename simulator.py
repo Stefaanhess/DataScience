6caaa774b6152b83e56d2938dbeb028e6d6aebb4
Stefaan <hessmann.stefaan@googlemail.com>
2017-07-24 16:03:29 +0200

    invert velocity at boxedge

--- simulator.py
+++ simulator.py
@@ -11,8 +11,9 @@ class Agent:
 x_list = []
 y_list =[]
 agents = []
-w = 0.5
-N = 4
+w = 0.75
+N = 20
+box_size = 10
    
 ### Updating the agents    
 
@@ -44,6 +45,12 @@ def go_towards_neighbour(aj):
     ai = find_nearest_neighbor(aj)
     aj.velo_temp = aj.velo + w * np.array([ai.pos[0] - aj.pos[0], ai.pos[1] - ai.pos[1]])
     aj.velo_temp = (aj.velo_temp / np.linalg.norm(aj.velo_temp))
+    pos_temp = aj.pos + aj.velo_temp
+    if is_outside_box(pos_temp):
+        print(aj.pos)
+        print(aj.velo_temp)
+        aj.velo_temp = invert_velocity(aj)
+        print(aj.velo_temp)
     aj.pos += aj.velo_temp   
     return
 
@@ -54,6 +61,21 @@ def update_agents():
     for ak in agents:
         ak.velo = ak.velo_temp
     return
+
+def invert_velocity(agent):
+    pos = agent.pos
+    velo = agent.velo_temp
+    if np.abs(pos[0]) < np.abs(pos[1]):
+        inv_index = 1
+    else:
+        inv_index = 0
+    velo[inv_index] *= -1
+    return velo
+
+def is_outside_box(pos):
+    if np.abs(pos[0]) > box_size or np.abs(pos[1]) > box_size:
+        return True
+    return False
 
 ### Plotting the simulation
 
@@ -73,25 +95,25 @@ def draw_simulation():
     ax.cla()
     ax.scatter(get_x_pos(), get_y_pos())
     plt.draw()
-    ax.set_xlim([-20, 20])
-    ax.set_ylim([-20, 20])
+    ax.set_xlim([-box_size, box_size])
+    ax.set_ylim([-box_size, box_size])
     return 
+
 
 ### Simulation
 
 initialize()
 
-fig = plt.figure(figsize=(10,10))
+fig = plt.figure(figsize=(6,6))
 ax = fig.add_subplot(111)
-ax.set_xlim([-20, 20])
-ax.set_ylim([-20, 20])
+ax.set_xlim([-box_size, box_size])
+ax.set_ylim([-box_size, box_size])
 
 def do_simulation():
     for i in range(100):
         update_agents()
         draw_simulation()
-        time.sleep(1)
-
+        time.sleep(0.1)
 
 t = threading.Thread(target=do_simulation)
 t.start()
