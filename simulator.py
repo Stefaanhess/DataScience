a7fa9d6acd0cf473fe6adbcbdb923a31f08b7cc0
Tom Burgert <tomburgert1@Toms-MacBook-Air.local>
2017-07-24 16:17:58 +0200

    Updated Graphics wihtout plot

--- simulator.py
+++ simulator.py
@@ -1,42 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import threading,time
+from graphics.graphics import *
 
 class Agent: 
     def __init__(self, pos, velo):
         self.pos = pos
         self.velo = velo
         self.velo_temp = velo
+        self.Point(pos[0],pos[1])
 
 x_list = []
 y_list =[]
 agents = []
-w = 0.5
-N = 20
+w = 2
+N = 100
+winWidth = 1000
+winHeight = 1000
+
    
 ### Updating the agents    
 
 def initialize():
     for i in range(N):
-        agents.append(Agent(10*np.random.random(2)-5,2*np.random.random(2)-1
+        agents.append(Agent(winWidth*np.random.random(2),4*np.random.random(2)-2))
+
+# draw the agents into the window to display them
+def draw_agents():
+    for ai in agents:
+        ai.Point.draw(window)
 
 def find_nearest_neighbor(aj): 
     distances = [np.linalg.norm(aj.pos - ai.pos) for ai in agents]
     distances[distances.index(0.0)] = max(distances)
     return(agents[distances.index(min(distances))])
 
+# rename it to update velocity
 def similar_direction_as_neighbour(aj):
     ai = find_nearest_neighbor(aj)
     aj.velo_temp = aj.velo + w*ai.velo
     aj.velo_temp = (aj.velo_temp / np.linalg.norm(aj.velo_temp))
-    aj.pos += aj.velo_temp    
     return 
 
+# rename it to update velocity
 def go_towards_neighbour(aj):
     ai = find_nearest_neighbor(aj)
     aj.velo_temp = aj.velo + w * np.array([ai.pos[0] - aj.pos[0], ai.pos[1] - ai.pos[1]])
     aj.velo_temp = (aj.velo_temp / np.linalg.norm(aj.velo_temp))
-    aj.pos += aj.velo_temp   
     return
 
 def update_agents():
@@ -45,46 +55,37 @@ def update_agents():
         go_towards_neighbour(aj)
     for ak in agents:
         ak.velo = ak.velo_temp
+        move_agent(ak)
     return
 
-### Plotting the simulation
+# move the agent according to the velocity, do not exceed boundaries
+def move_agent(aj):    
+    x = aj.Point.getX()
+    y = aj.Point.getY()
 
-def get_x_pos():
-    ret_list = []
-    for aj in agents:
-        ret_list.append(aj.pos[0])
-    return ret_list
+    if x > 0 and y < winHeight and x < winWidth and y > 0:
+        aj.Point.move(aj.velo[0],aj.velo[1])
 
-def get_y_pos():
-    ret_list = []
-    for aj in agents:
-        ret_list.append(aj.pos[1])
-    return ret_list
+    elif x <= 0 or x >= winWidth:
+        aj.Point.move(aj.velo[0]*(-1), aj.velo[1])
 
-def draw_simulation():
-    ax.cla()
-    ax.scatter(get_x_pos(), get_y_pos())
-    plt.draw()
-    ax.set_xlim([-20, 20])
-    ax.set_ylim([-20, 20])
-    return 
+    elif y <= 0 or y >= winHeight:
+        aj.Point.move(aj.velo[0], aj.velo[1]*(-1))
 
 ### Simulation
-
-initialize()
-
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(111)
-ax.set_xlim([-20, 20])
-ax.set_ylim([-20, 20])
 
 def do_simulation():
     for i in range(100):
         update_agents()
-        draw_simulation()
-        time.sleep(1)
+        time.sleep(0.3)
+
+    window.getMouse()
+    window.close()    
+
+window = GraphWin("Window", winWidth, winHeight)
+
+initialize()
+draw_agents()
+do_simulation()
 
 
-t = threading.Thread(target=do_simulation)
-t.start()
-plt.show()
