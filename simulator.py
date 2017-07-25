8324916c265242421c725889c3c7bb350d3f04d6
Tom Burgert <tomburgert1@Toms-MacBook-Air.local>
2017-07-25 16:25:37 +0200

    Code with Algorithm Couzin from Class 2, now actually working, without noise function

--- simulator.py
+++ simulator.py
@@ -6,28 +6,32 @@ import time
 import random
 import math
 
+### CLASS AGENT
+
 class Agent:
     def __init__(self, pos, velo):
         self.velo = velo
         self.velo_temp = velo
         self.point = Circle(Point(pos[0],pos[1]),3)
 
-agents = []
-w = 2
-N = 40
+### GLOBAL PARAMETERS
 
+agents = []
+N = 40
 winWidth = 1000
 winHeight = 1000
+
+# turning points at border
 minB = 15
 maxB = winWidth-15
 
 window = GraphWin("Window", winWidth, winHeight)
    
-### Setting up the world    
+### SETTING UP THE WORLD    
 
 def initialize():
     for i in range(N):
-        agents.append(Agent(winWidth/2*np.random.random(2)+winWidth*(1/4),2*np.random.random(2)-1))
+        agents.append(Agent(winWidth/2*np.random.random(2)+winWidth*(1/4),normalize(4*np.random.random(2)-2)))
     for ai in agents:
         ai.point.setFill("red")
 
@@ -36,8 +40,25 @@ def draw_agents():
     for ai in agents:
         ai.point.draw(window)
 
+# function to avoid border crossing
+def avoid_border_crossing(aj):
+    if aj.point.getCenter().getX() <= minB or aj.point.getCenter().getX() >= maxB:
+        aj.velo_temp[0] = aj.velo_temp[0]*(-1)
+    elif aj.point.getCenter().getY() <= minB or aj.point.getCenter().getY() >= maxB:
+        aj.velo_temp[1] = aj.velo_temp[1]*(-1)
 
-### Simpel Update Function (Class 1)
+# normalizing function to define speed of agents
+def normalize(vec):
+    return(norm*vec/np.linalg.norm(vec))
+
+
+
+
+### SIMPEL FUNCTION DAY 1 
+### MOUDLE 1
+
+# weight parameter
+w = 2
 
 # nearest neighbor funtion
 def find_nearest_neighbuor(aj): 
@@ -58,19 +79,15 @@ def assimilate_velo(aj):
     elif aj.point.getCenter().getY() <= minB or aj.point.getCenter().getY() >= maxB:
         aj.velo_temp[1] = aj.velo_temp[1]*(-1)
 
-def avoid_border_crossing(aj):
-    if aj.point.getCenter().getX() <= minB or aj.point.getCenter().getX() >= maxB:
-        aj.velo_temp[0] = aj.velo_temp[0]*(-1)
-    elif aj.point.getCenter().getY() <= minB or aj.point.getCenter().getY() >= maxB:
-        aj.velo_temp[1] = aj.velo_temp[1]*(-1)
+### ALGORITHM OF COUZIN - CLASS 2
+### MODULE 2
 
-### Algorithm of Couzin (Class 2)
+# global parameter for all agents, all radius and speed
 
-# global parameter for all agents
-
-Rr = 30
-Ro = 30
-Ra = 45
+Rr = 20
+Ro = 60
+Ra = 90
+norm = 5
 
 # calculate all distances to all agents
 def calculate_distances(aj):
@@ -107,23 +124,22 @@ def calculate_direction_R(dist, pos_dif):
     direct = np.zeros(2)
     for i in range(len(dist)):
         if (dist[i] < Rr and dist[i] > 0.0):
-            direct = direct - (pos_dif[i]/dist[i])
+            direct = direct + normalize((pos_dif[i]/dist[i]))
     return direct
 
 def calculate_direction_O(dist):
     direct = np.zeros(2)
     for i in range(len(dist)):
         if (dist[i] >= Rr and dist[i] < Ro):
-            direct += agents[i].velo/np.linalg.norm(agents[i].velo)
+            direct += normalize(agents[i].velo/np.linalg.norm(agents[i].velo))
     return direct
 
 def calculate_direction_A(dist, pos_dif):
     direct = np.zeros(2)
     for i in range(len(dist)):
         if (dist[i] >= Ro and dist[i] < Ra):
-            direct += pos_dif[i]/dist[i]
+            direct = direct - normalize((pos_dif[i]/dist[i]))
     return direct
-
 
 # asses next step for agent
 
@@ -133,24 +149,24 @@ def asses_next_step(aj):
 
     if (neighbours_in_zoneR(dist)==True):
         new_direct = calculate_direction_R(dist, pos_dif)
-        print("Fall 1")
-##        
-##    elif (neighbours_in_zoneO(dist)==True and neighbours_in_zoneA(dist)==True):
-##        new_direct = 0.5*(calculate_direction_A(dist, pos_dif) + calculate_direction_O(dist))
-## #       print("Fall 2")
-##
-##    elif (neighbours_in_zoneO(dist)==True):
-##        new_direct = calculate_direction_O(dist)
-## #      print("Fall 3")
-##
-##    elif (neighbours_in_zoneA(dist)==True):
-##        new_direct = calculate_direction_A(dist, pos_dif)
-## #       print("Fall 4")
+        
+    elif (neighbours_in_zoneO(dist)==True and neighbours_in_zoneA(dist)==True):
+        new_direct = 0.5*(calculate_direction_A(dist, pos_dif) + calculate_direction_O(dist))
+
+    elif (neighbours_in_zoneO(dist)==True):
+        new_direct = calculate_direction_O(dist)
+
+    elif (neighbours_in_zoneA(dist)==True):
+        new_direct = calculate_direction_A(dist, pos_dif)
 
     else:
         new_direct = np.zeros(2)
-        
-    aj.velo_temp = aj.velo + new_direct
+
+    aj.velo_temp = normalize(aj.velo + new_direct)
+
+
+### END OF MODULE 2
+
 
 # move the agent according to the velocity
 def move_agent(aj):
@@ -172,10 +188,8 @@ def update_agents():
 ### Simulation
 
 def do_simulation():
-    for i in range(1000):
+    for i in range(3000):
         update_agents()
- #       time.sleep(0.01)
-
     window.getMouse()
 
 ### RUN IT
@@ -183,9 +197,3 @@ def do_simulation():
 initialize()
 draw_agents()
 do_simulation()
-
-### Some Methods names
-### Point exchanged with Circle to better illustrate
-### Random Noise
-
-### NOW: How to avoid all sticking to the borders?
