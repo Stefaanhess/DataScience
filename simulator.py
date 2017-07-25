8fe81d58f2f85443e46cf3613af071b5c1c06ba8
Tom Burgert <tomburgert1@Toms-MacBook-Air.local>
2017-07-25 14:17:44 +0200

    Code with Algorithm Couzin from Class 2

--- simulator.py
+++ simulator.py
@@ -15,15 +15,19 @@ class Agent:
 agents = []
 w = 2
 N = 40
+
 winWidth = 1000
 winHeight = 1000
+minB = 15
+maxB = winWidth-15
+
 window = GraphWin("Window", winWidth, winHeight)
    
 ### Setting up the world    
 
 def initialize():
     for i in range(N):
-        agents.append(Agent(winWidth*np.random.random(2),4*np.random.random(2)-2))
+        agents.append(Agent(winWidth*np.random.random(2),8*np.random.random(2)-4))
     for ai in agents:
         ai.point.setFill("red")
 
@@ -33,7 +37,7 @@ def draw_agents():
         ai.point.draw(window)
 
 
-### Updating Agents
+### Simpel Update Function (Class 1)
 
 # nearest neighbor funtion
 def find_nearest_neighbuor(aj): 
@@ -49,10 +53,100 @@ def assimilate_velo(aj):
     aj.velo_temp = (aj.velo_temp / np.linalg.norm(aj.velo_temp)*10)
 
     # avoiding boarder crossing
-    if aj.point.getCenter().getX() <= 0 or aj.point.getCenter().getX() >= winWidth:
+    if aj.point.getCenter().getX() <= minB or aj.point.getCenter().getX() >= maxB:
         aj.velo_temp[0] = aj.velo_temp[0]*(-1)
-    elif aj.point.getCenter().getY() <= 0 or aj.point.getCenter().getY() >= winHeight:
+    elif aj.point.getCenter().getY() <= minB or aj.point.getCenter().getY() >= maxB:
         aj.velo_temp[1] = aj.velo_temp[1]*(-1)
+
+### Algorithm of Couzin (Class 2)
+
+### Behavourial Rules of Zoning
+
+# global parameter for all agents
+
+Rr = 10
+Ro = 40
+Ra = 80
+winWidth = 1000
+
+# calculate all distances to all agents
+def calculate_distances(aj):
+    pos_dif = [ np.array([aj.point.getCenter().getX(), aj.point.getCenter().getY()]) -
+                np.array([ai.point.getCenter().getX(), ai.point.getCenter().getY()]) for ai in agents]
+    dist = np.linalg.norm(np.array(pos_dif),axis=0)
+    return([dist, pos_dif])
+
+
+# check for agents in repulsion, orientation or attraction zone
+
+def neighbours_in_zoneR(distances):
+     for i in range(len(distances)):
+        if(distances[i] > 0 and distances[i] < Rr):
+            return True
+        else:
+            return False
+
+def neighbours_in_zoneO(distances):
+    for i in range(len(distances)):
+        if(distances[i] >= Rr and distances[i] < Ro):
+            return True
+        else:
+            return False
+        
+def neighbours_in_zoneA(distances):
+    for i in range(len(distances)):
+        if(distances[i] >= Ro and distances[i] < Ra):
+            return True
+        else:
+            return False
+
+
+# calculate new direction according to agents in zones
+
+def calculate_direction_R(dist, pos_dif):
+    direct = np.zeros(2)
+    for i in range(len(dist)):
+        if (dist[i] < Rr and dist[i] > 0):
+            direct -= pos_dif[i]/dist[i]
+    return direct
+
+def calculate_direction_O(dist):
+    direct = np.zeros(2)
+    for i in range(len(dist)):
+        if (dist[i] >= Rr and dist[i] < Ro):
+            direct += agents[i].velo/np.linalg.norm(agents[i].velo)
+    return direct
+
+def calculate_direction_A(dist, pos_dif):
+    direct = np.zeros(2)
+    for i in range(len(dist)):
+        if (dist[i] >= Ro and dist[i] < Ra):
+            direct += pos_dif[i]/dist[i]
+    return direct
+
+
+# asses next step for agent
+
+def asses_next_step(aj):
+    dist = calculate_distances(aj)[0]
+    pos_dif = calculate_distances(aj)[1]
+
+    if (neighbours_in_zoneR(dist)):
+        new_direct = calculate_direction_R(dist, pos_dif)
+        
+    elif (neighbours_in_zoneO(dist) and neighbours_in_zoneA(dist)):
+        new_direct = 0.5*(calculate_direction_A(dist, pos_dif) + calculate_direction_O(dist))
+
+    elif (neighbours_in_zoneO(dist)):
+        new_direct = calculate_direction_O(dist)
+
+    elif (neighbours_in_zoneA(dist)):
+        new_direct = calculate_direction_A(dist, pos_dif)
+
+    else:
+        new_direct = np.zeros(2)
+
+    aj.velo_temp = aj.velo + new_direct
 
 # move the agent according to the velocity
 def move_agent(aj):
@@ -62,7 +156,10 @@ def move_agent(aj):
 # second loop: set value old velo to new velo and move agents
 def update_agents():
     for aj in agents:
-        assimilate_velo(aj);
+        # Algo 1
+ #       assimilate_velo(aj);
+        # Algo 2
+        asses_next_step(aj)
     for ai in agents:
         ai.velo = ai.velo_temp
         move_agent(ai)
@@ -70,7 +167,7 @@ def update_agents():
 ### Simulation
 
 def do_simulation():
-    for i in range(100):
+    for i in range(1000):
         update_agents()
  #       time.sleep(0.01)
 
