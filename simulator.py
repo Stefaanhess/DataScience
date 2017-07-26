91eec4fb5ea10d5552f96782c0ad9cc6c4b52135
Tom Burgert <tomburgert1@Toms-MacBook-Air.local>
2017-07-26 16:12:09 +0200

    Allowance of Border Crossing implemented

--- simulator.py
+++ simulator.py
@@ -84,6 +84,16 @@ def avoid_border_crossing(aj):
     elif aj.point.getCenter().getY() <= minB or aj.point.getCenter().getY() >= maxB:
         aj.velo_temp[1] = aj.velo_temp[1]*(-1)
 
+# function to allow border crossing (not the case of the exact corner problem!)
+def allow_border_crossing(aj):
+    if aj.point.getCenter().getX() <= 0:
+        aj.point.move(winWidth,0)
+    elif  aj.point.getCenter().getX() >= winWidth:
+        aj.point.move(-winWidth,0)
+    elif  aj.point.getCenter().getY() <= 0:
+        aj.point.move(0,winHeight)
+    elif  aj.point.getCenter().getY() >= winHeight:
+        aj.point.move(0,-winHeight)
 
 # move the agent according to the velocity
 def move_agent(aj):
@@ -100,7 +110,7 @@ def update_agents():
         # Algo 3
         vicek_next_step(aj, agents, norm)
     for ai in agents:
-        avoid_border_crossing(ai)
+        allow_border_crossing(ai)
         ai.velo = ai.velo_temp
         move_agent(ai)
 
